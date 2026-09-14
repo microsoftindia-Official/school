483c636da21544d194c98e887e14166f78b40069
microsoftindia-Official <minecrosoftindia@gmail.com>
2026-09-14 08:17:20 +0200

    Add files via upload

---
+++ L1.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>witaj swiecie</p>
+
+    <script>
+        // skrupt lavascrpit
+    </script>
+    <?php
+        // echo "<p>witaj swiecie 2</p>";
+
+        // $imie = "Alan";
+        // $nazwisko = "Jaworek";
+        // $wiek = 42;
+        // $wzrost 150.3;
+        
+        // echo "<p>Imię:".$imie;."<br>Nazwisko:" .$nazwisko."<br>Wiek:".$wiek."
+        // lat<br>Wzrost: ".wzrost
+        // $a = 55;
+        // $b = 11;
+        // echo "<p>Suma liczb" .$a." i ".$b." wynosi ".$a+$b. "</p>";
+        // echo "<p>Różnica liczb" .$a." i ".$b." wynosi ".$a-$b. "</p>";
+        // echo "<p>Iloczyn liczb" .$a." i ".$b." wynosi ".$a*$b. "</p>";
+        // echo "<p>Iloraz liczb" .$a." i ".$b." wynosi ".$a/$b. "</p>";
+        // echo "<p>Reszta z dzielenia (modulo) liczb" .$a." i ".$b." wynosi ".$a%$b. "</p>";\
+
+        $a = 23;
+
+        echo $a . "\n";
+        echo -$a . "\n";
+        echo ($a + 3) . "\n";
+        echo ($a - 5) . "\n";
+        echo ($a * 2) . "\n";
+        echo ($a / 5) . "\n";
+        echo ($a % 2) . "\n";
+        echo ($a ** 3) . "\n";
+        echo "Wartość zmiennej $a wynosi: " . $a . " i już się nie zmieni.\n";
+
+    ?>
+</body>
+</html>
